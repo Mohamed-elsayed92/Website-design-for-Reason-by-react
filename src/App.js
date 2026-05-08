f481de116b6822756b17ceff02d34342fb2611e0
Mohamed-elsayed92 <me4379421@gmail.com>
2026-05-08 14:48:19 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
     import './App.css';
-    import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
     import 'bootstrap/dist/css/bootstrap.min.css';
     import 'bootstrap/dist/js/bootstrap.bundle.min.js';
     import Navbar from './components/01-Navbar';
